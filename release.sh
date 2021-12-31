bfbeec0695d273b6fb8ca47996d7152a6ee74ef3
webdiver <azagarov@mail.ru>
2021-12-31 14:19:00 +0300

    Fonts URL Releasing Script

--- release.sh
+++ release.sh
@@ -1,3 +1,3 @@
-cat css/tint-embed.css | sed 's_(/css/fonts/fa-_(/t/w/tint-embed-assets-B833i/css/fonts/fa-_g' > css/tint-embed-new.css
+cat css/tint-embed.css | sed 's_(/css/fonts/fa-_(//cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/webfonts/fa-_g' > css/tint-embed-new.css
 mv css/tint-embed-new.css css/tint-embed.css
 
